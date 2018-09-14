73b603b5063fab47c6076584b1cc53ae96deba36
Mustapha Adubiagbe <mustaphababajide@Mustaphas-MBP.hitronhub.home>
2018-09-14 13:01:37 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+import { ApolloServer, gql } from "apollo-server";
+import { MvrpAPI } from './datasource'
+
+const typeDefs = gql`
+  type Car {
+      id: Int!
+      plateNumber: String!
+      color: String!
+      model: String!
+      chasisNumber: String!
+      vehicleStatus: String!
+      yearOfManufacture: Int!
+      issueDate: String!
+      expiryDate: String!
+  }
+  type Query {
+      car(plateNumber: String!): Car
+      cars: [Car]
+  }
+`;
+
+const resolvers = {
+    Query: {
+        car: (root, { plateNumber }, { dataSources }) => dataSources.mvrpAPI.getCar(plateNumber),
+        cars: (root, args, { dataSources }) => dataSources.mvrpAPI.getAllCars(),
+    },
+    Car: {
+        vehicleStatus: ({ status }) => status,
+        yearOfManufacture: ({ productionYear }) => productionYear
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => ({
+        mvrpAPI: new MvrpAPI
+    })
+});
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`)
+})
